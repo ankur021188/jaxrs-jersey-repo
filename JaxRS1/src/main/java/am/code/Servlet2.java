59729593d522345ad98e71b4fa8b9d6e7e7e0ee6
user <user@DESKTOP-T73COL1>
2019-07-13 17:18:17 +0530

    Response change for form submit

--- JaxRS1/src/main/java/am/code/Servlet2.java
+++ JaxRS1/src/main/java/am/code/Servlet2.java
@@ -3,14 +3,13 @@ package am.code;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/save")
 public class Servlet2 {
 
 	@POST
-	public String postTest(@FormParam("id") int id, @FormParam("name") String name, @FormParam("price") float price) {
-		return "<table><tr><td>Id</td><td>Name</td><td>Price</td></tr><tr><td>"+id+"</td><td>"+name+"</td><td>"+price+"</td></tr></table>";
+	public Response postTest(@FormParam("id") int id, @FormParam("name") String name, @FormParam("price") float price) {
+		return Response.status(200).entity("<table><tr><td>Id</td><td>Name</td><td>Price</td></tr><tr><td>"+id+"</td><td>"+name+"</td><td>"+price+"</td></tr></table>").build();
 	}
 }
